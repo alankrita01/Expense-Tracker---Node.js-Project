276462769c8135c5851aef115dd74edecb27e484
Alankrita Patidar <2102561>
2023-03-27 21:56:01 +0530

    updating public folder

---
+++ public/signup.js
@@ -0,0 +1,27 @@
+async function signup(event){
+  try{
+    event.preventDefault();
+    console.log(event.target.email.value);
+
+    const signupDetails = {
+      fullName : event.target.fullName.value,
+      email : event.target.email.value,
+      password: event.target.password.value
+    }
+
+    console.log(signupDetails)
+    const response = await axios.post('http://localhost:3000/user/signup',signupDetails)
+    
+
+   /* if(response.status === 201){
+      window.location.href = "../Login/login.html"
+    }
+    else{
+      throw new Error('Failed to login')
+    }*/
+  }
+  catch(err){
+    document.body.innerHTML += `<div style ="color:red;>${err}</div>`;
+    console.log(err);
+  }
+}
