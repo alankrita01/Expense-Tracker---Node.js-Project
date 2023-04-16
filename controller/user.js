f73586ee10dbe1434dc2a419b728f5c025b35440
Alankrita Patidar <2102561>
2023-04-16 14:28:59 +0530

    updating user.js controller file

--- controller/user.js
+++ controller/user.js
@@ -1,11 +1,24 @@
 const User = require('../model/userModel');
 
+function isstringinvalid(string){
+  if(string == undefined || string.length === 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
 const postSignup = async (req,res,next) => {
 
   try{
     const fullName = req.body.fullName;
     const email = req.body.email;
     const password = req.body.password;
+    console.log('email', email)
+    if(isstringinvalid(fullName)|| isstringinvalid(email) || isstringinvalid(password)){
+      return res.status(400).json({err: "Bad parameters. Something is missing"})
+    }
 
     await User.create({
       fullName: fullName,
@@ -15,14 +28,44 @@ const postSignup = async (req,res,next) => {
     res.status(201).json({message: 'Sign Up Successful'});
   }
   catch(err){
-    console.log("error occur in Add-user",JSON.stringify(err));
+    console.log("error occur in Signup",err.stack);
     res.status(500).json({
-      error: "error occur in Add-user"
+      error: "error occur in Signup"
     })
   }
 }
 
 
+const postLogin = async (req,res,next) => {
+  try{
+    const email = req.body.email;
+    const password = req.body.password;   //collecting email and password from the body
+   
+    if(isstringinvalid(email) || isstringinvalid(password)){
+      return res.status(400).json({message: " emailId or password is missing", success: false})
+    }
+    console.log(password);
+  
+    const user = await User.findAll({where : {email}})
+      if(user.length > 0){
+        if(user[0].password === password){
+          res.status(200).json({success: true, message: "User logged in Successfully"})
+        }
+        else{
+          return res.status(400).json({success:false, message: "Password is incorrect"})
+        }
+      }
+      else{
+        return res.status(404).json({success: false, message: "User Does not exist"})
+      }
+  }
+  catch(err){
+    console.log("error occur while login",err.stack);
+    res.status(500).json({success:false, message: err})
+  }
+}
+
 module.exports = {
-  postSignup
+  postSignup,
+  postLogin
 }
